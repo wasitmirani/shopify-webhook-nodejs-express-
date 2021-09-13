a257485fdca8010992ca6febc585d12c99830fd8
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-09-13 14:30:17 +0500

    update nodejs

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let app = require('express')();
-
-let http = require('http').Server(app);
+const express = require('express');
 const bodyParser = require('body-parser');
 
-
+const app = express();
+const port =process.env.PORT || 3000;
 
 /*
 Shopify issues a HTTP POST request.
@@ -16,25 +15,34 @@ app.use(bodyParser.urlencoded({
 
 
 
+app.get('/',(req, res) => {
 
-
-
-
-
-
-process.env.PORT=80;
-app.get('/', (req, res) => {
-   return res.json("socket server connected");
-})
-
+res.send("this is nodejs");
+});
+/*
+In your Shopify store under..
+Settings->Notifications
+Click "Create a webhook"
+- Choose your event
+- Leave the format as JSON
+- set the URL with your IP address to your server so it's something like http://123.345.11.22:3800/your-path
+Then update the path "your-path" below to match, as well as the port number below.
+*/
 app.post('/webhook/order/create', (req, res) => {
-    // let Shopify know we received the order details ok
-    res.send('OK');
-  
-    // the body of the data received
-    const theData = req.body;
-    console.log(theData);
-  });
-http.listen(process.env.PORT, () => {
-    console.log('Server is started at http://localhost:'+process.env.PORT)
+  // let Shopify know we received the order details ok
+  res.send('OK');
+
+  // the body of the data received
+  const theData = req.body;
+  console.log(theData);
+});
+
+
+
+/*
+On your server run
+node listen-for-shopify-webhooks.js
+*/
+app.listen(port, () => {
+  console.log(`Listening for Shopify webhook event data on port ${port}. Started ${new Date().toString()}`);
 })
