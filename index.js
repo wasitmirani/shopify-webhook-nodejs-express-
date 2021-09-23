1392732812bb4ce0229e508ed77c991f3caaaa94
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-09-23 18:27:35 +0500

    order fliter order

--- index.js
+++ index.js
@@ -23,8 +23,35 @@ res.send("this is nodejs");
 app.post('/webhook/order/create', (req, res) => {
   
   res.send('OK');
-  const theData = req.body;
-  console.log(theData);
+  const res = req.body;
+  let products=res.line_items.map((item)=>{
+    const product={
+      'id':item.id,
+      'title':item.title,
+      'quantity':item.quantity,
+      'sku':item.sku,
+      'price':item.price,
+    }
+    return product;
+  });
+  let order;
+  order=[
+      {'order_id':res.id,
+       'email':res.email,
+       'cancel_reason':res.cancel_reason,
+       'cancelled_at':res.cancelled_at,
+       'created_at':res.created_at,
+       'current_subtotal_price':res.current_subtotal_price,
+       'current_total_price':res.current_total_price,
+       'order_number':res.order_number,
+       'phone':res.phone,
+       'total_discounts':res.total_discounts,
+       'order_status_url':res.order_status_url,
+       'note':res.note,
+       'total_price':res.total_price,
+       'products':products,
+      }];
+  console.log("order",order);
 });
 
 
